61ab2d0cb9fa1ae671f98d1ebcb36f1b00bfa926
TheBao2004 <baonhtph39681@gmail.com>
2024-07-30 13:41:47 +0700

    Lam modude tai khoan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\CategoriesSeeder;
 
@@ -18,11 +20,23 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // // ]);
+        // $this->call([
+        //     CategoriesSeeder::class,
+        //     BlogsSeeder::class,
+        //     ProductsSeeder::class
         // ]);
-        $this->call([
-            CategoriesSeeder::class,
-            BlogsSeeder::class,
-            ProductsSeeder::class
-        ]);
+
+            for ($i=1; $i <= 10; $i++) {
+                $item = new User();
+
+                $item->name = "Tài khoản $i";
+                $item->email = "taikhoan$i@gmail.com";
+                $item->password = '$2y$12$pCTHEFoz4v74xBX0qHTP2ecdhfQXzJA0mWkMbZgyh/0fYkShOEjp2';
+                $item->permission = 0;
+
+                $item->save();
+            }
+
     }
 }
